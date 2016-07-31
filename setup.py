9594c9c8e96d2eb3f2a2414615db89fa59e0df37
Jelle Aalbers <jelleaalbers@gmail.com>
2016-07-31 14:46:52 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 requirements = open('requirements.txt').read().splitlines()
 
 setup(name='wimpy',
-      version='0.1.0',
+      version='0.2.0',
       description='Unnamed Statistical Inference Support / Likelihood Creation package',
       long_description=readme + '\n\n' + history,
       author='Jelle Aalbers',
