09d38ccbfd35c6648544958b91a60d5b709341cb
Jelle Aalbers <jelleaalbers@gmail.com>
2016-08-17 09:40:49 +0200

    First attempt for radial interpolation

--- blueice/likelihood.py
+++ blueice/likelihood.py
@@ -31,50 +31,58 @@ class LogLikelihood(object):
         if likelihood_config is None:
             likelihood_config = {}
         self.config = likelihood_config
+        self.itp_mode = self.config.get('interpolation_mode', 'regular')
+        assert self.itp_mode in ('grid', 'radial')
 
         self.pdf_base_config = pdf_base_config
         self.base_model = None          # Base model: no variations of any settings
         self.rate_parameters = OrderedDict()     # sourcename_rate -> logprior
         self.shape_parameters = OrderedDict()    # settingname -> (anchors, logprior).
-                                                 # where anchors is dict: representative value -> actual setting
+                                                 # where anchors is dict: representative number -> actual setting
+                                                 # From here on representative number will be called 'z-score'
                                                  # We'll take care of sorting the keys in self.prepare()
         self.source_list = []
         self.is_prepared = False
         self.is_data_set = False
 
+        # In case there are NO shape parameters:
+        self.ps = None                # ps of the data
+
         # These are only used in case there are shape parameters
         self.mu_interpolator = None     # RegularGridInterpolator mapping z scores -> rates for each source
         self.ps_interpolator = None     # RegularGridInterpolator mapping z scores -> (source, event) p-values
-        # TODO: rename some variables / change some comments:
-        # These are no longer z-scores but the actual setting values, or their 'standins' for non-numerical settings
-        # (which may well be z-scores, but that's up to the user)
-        self.anchor_z_arrays = None     # list of numpy arrays of z-parameters of each anchor model
-        self.anchor_z_grid = None       # numpy array: z-parameter combinations grid
         self.anchor_models = dict()     # dictionary mapping z-score -> actual model
+        self.anchor_z_arrays = None     # list of numpy arrays of z-parameters of each anchor model
 
-        # These are only used in case there are NO shape parameters
-        self.ps = None                # ps of the data
+        # For the 'grid' interpolation mode:
+        self.anchor_z_grid = None       # numpy array: z-parameter combinations grid
 
         # Compute the base model.
         self.base_model = Model(self.pdf_base_config, ipp_client=ipp_client)
         self.source_list = [s.name for s in self.base_model.sources]
 
-    def prepare(self, ipp_client=None):
+    def prepare(self, n_sample_points=100, ipp_client=None):
         """Prepares a likelihood function with shape parameters for use.
         This will  compute the models for each shape parameters anchor value combination.
         """
         if not len(self.shape_parameters):
             return
 
-        # Compute the anchor grid
-        self.anchor_z_arrays = [np.array(list(sorted(anchors.keys())))
-                                for setting_name, (anchors, _) in self.shape_parameters.items()]
-        self.anchor_z_grid = arrays_to_grid(self.anchor_z_arrays)
+        if self.itp_mode == 'grid':
+            # Compute a regular grid of anchor models at the specified anchor points
+            self.anchor_z_arrays = [np.array(list(sorted(anchors.keys())))
+                                    for setting_name, (anchors, _) in self.shape_parameters.items()]
+            self.anchor_z_grid = arrays_to_grid(self.anchor_z_arrays)
+
+        else:
+            # Sample a Latin hypercube of models
+            self.anchor_z_arrays = latin(n_sample_points,
+                                         len(self.shape_parameters),
+                                         box=self.get_bounds())
 
         # Create the configs for each model
-        zs_list = [zs for _, zs in self.anchor_grid_iterator()]
         configs = []
-        for zs in zs_list:
+        for zs in self.anchor_z_arrays:
             # Construct the config for this model
             config = deepcopy(self.pdf_base_config)
             config['show_pdf_sampling_progress'] = False
@@ -85,7 +93,7 @@ class LogLikelihood(object):
         models = create_models_in_parallel(configs, ipp_client,
                                            block=self.config.get('block_during_paralellization', False))
 
-        for zs, model in tqdm(zip(zs_list, models),
+        for zs, model in tqdm(zip(self.anchor_z_arrays, models),
                              total=len(configs),
                              desc="Computing models for shape parameter anchor points"):
             self.anchor_models[zs] = model
@@ -159,13 +167,13 @@ class LogLikelihood(object):
                 if log_prior is not None:
                     result += log_prior(z)
 
-            # The RegularGridInterpolators want numpy arrays: give it to them...
-            zs = np.asarray([zs])
+                        # The RegularGridInterpolators want numpy arrays: give it to them...
+            zs = np.asarray(zs)
 
             # Get mus (rate for each source) and ps (pdf value for each source for each event) at this point
-            # The RegularGridInterpolators return numpy arrays with one extra dimension: remove it...
-            mus = self.mus_interpolator(np.array(zs))[0]
-            ps = self.ps_interpolator(zs)[0]
+                        # The RegularGridInterpolators return numpy arrays with one extra dimension: remove it...
+            mus = self.mus_interpolator(zs)
+            ps = self.ps_interpolator(zs)
 
         else:
             mus = self.base_model.expected_events()
@@ -209,19 +217,48 @@ class LogLikelihood(object):
         :param f: Function which takes a model as argument, and produces an extra_dims shaped array.
         :param extra_dims: tuple of integers, shape of return value of f.
         """
-        # Allocate an array which will hold the scores at each anchor model
-        anchor_scores = np.zeros(list(self.anchor_z_grid.shape)[:-1] + extra_dims)
+        if self.itp_mode == 'regular':
 
-        # Iterate over the anchor grid points
-        for anchor_grid_index, zs in self.anchor_grid_iterator():
+            # Allocate an array which will hold the scores at each anchor model
+            anchor_scores = np.zeros(list(self.anchor_z_grid.shape)[:-1] + extra_dims)
 
-            # Compute f at this point, and store it in anchor_scores
-            anchor_scores[anchor_grid_index + [slice(None)] * len(extra_dims)] = f(self.anchor_models[tuple(zs)])
+            # Iterate over the anchor grid points
+            for anchor_grid_index, _zs in self.anchor_grid_iterator():
 
-        return RegularGridInterpolator(self.anchor_z_arrays, anchor_scores)
+                # Compute f at this point, and store it in anchor_scores
+                anchor_scores[anchor_grid_index + [slice(None)] * len(extra_dims)] = f(self.anchor_models[tuple(_zs)])
 
-    def get_bounds(self, parameter_name):
+            return RegularGridInterpolator(self.anchor_z_arrays, anchor_scores)
+
+        elif self.itp_mode == 'radial':
+            anchor_scores = np.array([f(_models_zs) for _models_zs in self.anchor_z_arrays])
+
+            # Rescale the zs to the bounds. These could have been stored as attributes in prepare() instead...
+            bounds = np.array(self.get_bounds())
+            mins = bounds[:, 0]
+            lengths = bounds[:, 1] - bounds[:, 0]
+            models_zs = [(_zs - mins)/lengths for _zs in self.anchor_z_arrays]
+            r0 = approx_avg_distance(models_zs)
+
+            def interpolator(zs):
+                # Closure: Gets mins, lengths, model_zs, r0 from outer scope
+
+                # Compute the distance between the current point and each model
+                zs = (zs - mins) / lengths
+                rs = np.sqrt([np.dot(zs, _zs) for _zs in models_zs])
+
+                # Compute the weight of each model: inverse multi-quadratic
+                weights = 1/np.sqrt(1+(rs/r0)**2)
+                weights /= np.sum(weights)
+
+                return np.average(anchor_scores, weights=anchor_scores, axis=0)
+
+            return interpolator
+
+    def get_bounds(self, parameter_name=None):
         """Return bounds on the parameter parameter_name"""
+        if parameter_name is None:
+            return [self.get_bounds(p) for p in self.shape_parameters.keys()]
         if parameter_name in self.shape_parameters:
             anchor_settings = list(self.shape_parameters[parameter_name][0].keys())
             return min(anchor_settings), max(anchor_settings)
@@ -270,3 +307,61 @@ def arrays_to_grid(arrs):
     """Convert a list of n 1-dim arrays to an n+1-dim. array, where last dimension denotes coordinate values at point.
     """
     return np.stack(np.meshgrid(*arrs, indexing='ij'), axis=-1)
+
+
+def approx_avg_distance(points):
+    """Some approximation to average distance between points (array (n_points, n_dim))
+    found in scipy: https://github.com/pv/scipy-work/blob/master/scipy/interpolate/rbf.py#L203
+    """
+    # default epsilon is the "the average distance between nodes" based
+    # on a bounding hypercube
+    ximax = np.amax(points, axis=1)
+    ximin = np.amin(points, axis=1)
+    edges = ximax-ximin
+    edges = edges[np.nonzero(edges)]
+    return np.power(np.prod(edges)/len(points), 1.0/edges.size)
+
+
+def latin(n, d, box=None, shuffle_steps=1000):
+    """Creates a latin hypercube of n points in d dimensions
+    Stolen from https://github.com/paulknysh/blackbox
+    """
+    # starting with diagonal shape
+    pts=np.ones((n,d))
+
+    for i in range(n):
+        pts[i]=pts[i]*i/(n-1.)
+
+    # spread function
+    def spread(p):
+        s=0.
+        for i in range(n):
+            for j in range(n):
+                if i > j:
+                    s=s+1./np.linalg.norm(np.subtract(p[i],p[j]))
+        return s
+
+    # minimizing spread function by shuffling
+    currminspread=spread(pts)
+
+    for m in tqdm(range(shuffle_steps), desc='Shuffling latin hypercube'):
+
+        p1=np.random.randint(n)
+        p2=np.random.randint(n)
+        k=np.random.randint(d)
+
+        newpts=np.copy(pts)
+        newpts[p1,k],newpts[p2,k]=newpts[p2,k],newpts[p1,k]
+        newspread=spread(newpts)
+
+        if newspread<currminspread:
+            pts=np.copy(newpts)
+            currminspread=newspread
+
+    if box is None:
+        return pts
+
+    for i in range(len(box)):
+        pts[:, i] = box[i][0] + pts[:, i] * (box[i][1] - box[i][0])
+
+    return pts
